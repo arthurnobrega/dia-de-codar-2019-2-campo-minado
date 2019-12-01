929882f96bd8a06a6a86ca7802694f5c17f85a64
Arthur Nobrega <arthurthiago@gmail.com>
2019-12-01 12:04:28 -0300

    1575212668

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,33 +6,37 @@ RSpec.describe Board do
     expect(board).to be_kind_of(Board)
   end
 
-  it 'responds to #rows' do
-    board = Board.new
-    expect(board).to respond_to(:rows)
+  describe '#rows' do
+    it 'responds to #rows' do
+      board = Board.new
+      expect(board).to respond_to(:rows)
+    end
+
+    it 'has 8 rows by default' do
+      board = Board.new
+      expect(board.rows).to equal(8)
+    end
+
+    it 'changes default number of rows' do
+      board = Board.new(rows: 10)
+      expect(board.rows).to equal(10)
+    end
   end
 
-  it 'has 8 rows by default' do
-    board = Board.new
-    expect(board.rows).to equal(8)
-  end
+  describe '#columns' do
+    it 'responds to #columns' do
+      board = Board.new
+      expect(board).to respond_to(:columns)
+    end
 
-  it 'changes default number of rows' do
-    board = Board.new(rows: 10)
-    expect(board.rows).to equal(10)
-  end
+    it 'has 8 columns by default' do
+      board = Board.new
+      expect(board.columns).to equal(8)
+    end
 
-  it 'responds to #columns' do
-    board = Board.new
-    expect(board).to respond_to(:columns)
-  end
-
-  it 'has 8 columns by default' do
-    board = Board.new
-    expect(board.columns).to equal(8)
-  end
-
-  it 'changes default number of columns' do
-    board = Board.new(columns: 10)
-    expect(board.columns).to equal(10)
+    it 'changes default number of columns' do
+      board = Board.new(columns: 10)
+      expect(board.columns).to equal(10)
+    end
   end
 end
