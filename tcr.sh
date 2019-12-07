ac4764744d4dcf4d1cad15437bd604d288a73c22
Arthur Nobrega <arthurthiago@gmail.com>
2019-12-07 12:33:44 -0300

    Send commit message to tcr.sh

--- tcr.sh
+++ tcr.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
 
-TEST_OUTPUT=tmp/tcr_ouput.log
+TEST_OUTPUT=/tmp/tcr_ouput.log
 
 test () {
   rspec > $TEST_OUTPUT
@@ -11,7 +11,7 @@ commit () {
   cat $TEST_OUTPUT
   echo "Green!!!"
   git add -A
-  git commit -m "$(date +%s)"
+  git commit -m "$1"
   exit 0
 }
 
@@ -22,4 +22,4 @@ revert () {
   exit 1
 }
 
-test && commit || revert
+test && commit $1 || revert
